681293e2686246d40e86e98dee418c030fd70a78
arnold-jr <j.arnold.111@gmail.com>
2017-02-09 17:14:24 -0600

    Initial code commit

---
+++ src/WordCount.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+
+public class WordCount {
+
+    public static class TokenizerMapper
+    extends Mapper<Object, Text, Text, IntWritable>
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
